1e4ed0c4c7a557e7b7264536724bbe2464ee400d
Anchen <li.anchen.au@gmail.com>
2018-04-08 22:32:37 +1000

    Update get_data.sh

--- get_data.sh
+++ get_data.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 mkdir data
-wget http://sketch-code.s3.amazonaws.com/data/all_data.zip -O data/all_data.zip
+wget https://s3-us-west-2.amazonaws.com/sketch2code/data.zip -O data/all_data.zip
 unzip data/all_data.zip -d data/all_data
